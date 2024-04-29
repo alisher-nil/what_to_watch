c5f9f45694294e719c515d1c8f1cc853c449de1a
alisher-nil <alisher.nil@gmail.com>
2024-04-29 23:09:57 +0500

    fixed formatting with ruf

--- opinions_app.py
+++ opinions_app.py
@@ -3,9 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    return "Совсем скоро тут будет случайное мнение о фильме!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
