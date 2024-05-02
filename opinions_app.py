9d41bab4a07c18d35351749ff28658e354b35ab7
alisher-nil <alisher.nil@gmail.com>
2024-05-02 16:07:22 +0500

    add templates and static files

--- opinions_app.py
+++ opinions_app.py
@@ -1,4 +1,10 @@
-from flask import Flask
+# what_to_watch/opinions_app.py
+
+from datetime import datetime
+from random import randrange
+
+# Добавлена функция render_template
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,13 +12,37 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-
 db = SQLAlchemy(app)
+
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
 @app.route("/")
 def index_view():
-    return "Совсем скоро тут будет случайное мнение о фильме!"
+    quantity = Opinion.query.count()
+    if not quantity:
+        return "В базе данных мнений о фильмах нет."
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    return render_template("opinion.html", opinion=opinion)
+
+
+@app.route("/add")
+def add_opinion_view():
+    return render_template("add_opinion.html")
+
+
+@app.route("/opinions/<int:opinion_id>")
+def opinion_view(opinion_id):
+    opinion = Opinion.query.get(opinion_id)
+    print(opinion)
+    return render_template("opinion.html", opinion=opinion)
 
 
 if __name__ == "__main__":
