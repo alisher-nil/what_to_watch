8ebfd40fb04cfad0d8a2b5f355320e7458cbbf31
alisher-nil <alisher.nil@gmail.com>
2024-05-03 15:33:12 +0500

    add opinion add functionality

--- opinions_app.py
+++ opinions_app.py
@@ -1,7 +1,9 @@
+# what_to_watch/opinions_app.py
+
 from datetime import datetime
 from random import randrange
 
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
@@ -11,9 +13,17 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"] = "f9a7c58f-8be6-4e69-ac61-4a9ba220610e"
+app.config["SECRET_KEY"] = "MY SECRET KEY"
 
 db = SQLAlchemy(app)
+
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
 class OpinionForm(FlaskForm):
@@ -26,47 +36,48 @@ class OpinionForm(FlaskForm):
     )
     text = TextAreaField(
         "Напишите мнение",
-        validators=[
-            DataRequired(message="Обязательное поле"),
-        ],
+        validators=[DataRequired(message="Обязательное поле")],
     )
     source = URLField(
         "Добавьте ссылку на подробный обзор фильма",
-        validators=[
-            Length(1, 256),
-            Optional(),
-        ],
+        validators=[Length(1, 256), Optional()],
     )
     submit = SubmitField("Добавить")
-
-
-class Opinion(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), nullable=False)
-    text = db.Column(db.Text, unique=True, nullable=False)
-    source = db.Column(db.String(256))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
 @app.route("/")
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        return "В базе данных мнений о фильмах нет."
+        return "В базе данных записей нет."
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
     return render_template("opinion.html", opinion=opinion)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add_opinion_view():
     form = OpinionForm()
+    if form.validate_on_submit():
+        text = form.text.data
+        if Opinion.query.filter_by(text=text).first() is not None:
+            flash("Такое мнение уже было оставлено ранее")
+            return render_template("add_opinion.html", form=form)
+
+        opinion = Opinion(
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data,
+        )
+        db.session.add(opinion)
+        db.session.commit()
+        return redirect(url_for("opinion_view", id=opinion.id))
     return render_template("add_opinion.html", form=form)
 
 
-@app.route("/opinions/<int:opinion_id>")
-def opinion_view(opinion_id):
-    opinion = Opinion.query.get_or_404(opinion_id)
+@app.route("/opinions/<int:id>")
+def opinion_view(id):
+    opinion = Opinion.query.get_or_404(id)
     return render_template("opinion.html", opinion=opinion)
 
 
